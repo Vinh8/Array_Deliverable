a0c8140e1809ab254d76f499406bb822b7b59900
myste <myste@VINH>
2022-09-15 15:56:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Name: Vinh Huynh
+// Date: 09/15/2022
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+        int count = 25;
+        int[] fibonacci = new int[count];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (i >= 2)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+            
+            Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+        }
+    }
+}
